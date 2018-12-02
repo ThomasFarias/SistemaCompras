4469fc53ccd7eb023dfa2e906cceb79a8b86762b
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2018-12-02 19:55:05 -0300

    validacion al crear usuario
    valida que el email no este registrado y que las contraseñas sean iguales.

--- backEnd/serializer.py
+++ backEnd/serializer.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 
 
 from rest_framework.serializers import (
+    EmailField,
+    CharField,
     ModelSerializer,
     SerializerMethodField,
     ValidationError
@@ -11,16 +13,37 @@ from rest_framework.serializers import (
 User = get_user_model()
 
 class UsuarioSerializer(ModelSerializer):
+    username=CharField(label='Usuario')
+    password=CharField(label='Contraseña')
+    password2 =CharField(label='Confirmar contraseña')
+    email=EmailField(label='Correo Electronico')
     class Meta:
         model = User
+
         fields=[
             'username',
             'password',
+            'password2',
             'email',            
         ]
+        
         extra_kwargs={"password":
                         {"write_only":True}
                         }
+    def validate(self,data):
+        email=data['email']
+        user=User.objects.filter(email=email)
+        if user.exists():
+            raise ValidationError("Este correo ya esta vinculado a un usuario.")
+        return    
+    def validate_password2(self,value):
+        data=self.get_initial()
+        password1=data.get("password")
+        password2=value
+        if password1 != password2:
+            raise ValidationError("Las contraseñas debe coincidir")
+            return value
+        
     def create(self, validated_data):
         username=validated_data['username']
         email=validated_data['email']
